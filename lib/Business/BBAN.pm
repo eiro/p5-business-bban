6bd2d4c8cc6d96d2e984243173185cc6031f10fa
Marc Chantreux <khatar@phear.org>
2015-05-19 10:34:38 +0200

    podification of the javadoc

--- lib/Business/BBAN.pm
+++ lib/Business/BBAN.pm
@@ -1,40 +1,53 @@
-# ABSTRACT: helpers to compute BBAN key and generate an BBAN. 
-
 package Business::BBAN;  
 use Modern::Perl;
 our $VERSION = '0.0'; 
+# ABSTRACT: helpers to compute BBAN key and generate an BBAN.
+
+=head1 AUTHORS
+
+=over 4
+
+=item * 
+
+Vincent Lucas (code and documentation)
+
+=item * 
+
+Marc Chantreux (podification and cpan distrib)
+
+=back
 
 
+=head1 SYNOPSIS
 
-##
-# Package helper to compute BBAN key and generate an BBAN from a bank
-# identifier, a bank location identifier and an account identifier.
-#
-# @author Vincent Lucas
-##
+Package helper to compute BBAN key and generate an BBAN from a bank
+identifier, a bank location identifier and an account identifier.
 
-##
-# Converts alpha characters into digit following the described table (beware,
-# there is a gap between R and S):
-#
-#   Char        | Digit
-#   --------------------
-#   A, J        | 1
-#   B, K, S     | 2
-#   C, L, T     | 3
-#   D, M, U     | 4
-#   E, N, V     | 5
-#   F, O, W     | 6
-#   G, P, X     | 7
-#   H, q, Y     | 8
-#   I, R, Z     | 9
-#
-# @param bban   A string representation of the Basic Bank Account Number (BBAN),
-# which can contains some alpha caracters.
-#
-# @return A string representation of the Basic Bank Account Number (BBAN), which
-# contains only digits.
-##
+Converts alpha characters into digit following the described table (beware,
+there is a gap between R and S):
+
+    Char        | Digit
+    --------------------
+    A, J        | 1
+    B, K, S     | 2
+    C, L, T     | 3
+    D, M, U     | 4
+    E, N, V     | 5
+    F, O, W     | 6
+    G, P, X     | 7
+    H, q, Y     | 8
+    I, R, Z     | 9
+
+=head2 to_digit ( $bban )
+
+when C<$bban> is  string representation of the Basic Bank Account Number (BBAN),
+which can contains some alpha caracters.
+
+returns string representation of the Basic Bank Account Number (BBAN), which
+contains only digits.
+
+=cut
+
 sub to_digit (_) {
     my $bban = shift;
     $bban = uc $bban;
@@ -45,14 +58,17 @@ sub to_digit (_) {
     $bban;
 } 
 
-##
-# Computes the key corresponding to a given Basic Bank Account Number (BBAN)
-#
-# @param bban   A string representation of the Basic Bank Account Number (BBAN),
-# which contains only digits, but does not contains the key part.
-#
-# @return The computed key of the BBAN given in parameter.
-##
+=head2 compute_key ( $bban )
+
+Computes the key corresponding to a given Basic Bank Account Number (BBAN)
+
+when C<$bban> is a string representation of the Basic Bank Account Number (BBAN),
+which contains only digits, but does not contains the key part. 
+
+returns the computed key of the BBAN given in parameter. 
+
+=cut
+
 sub compute_key (_) {
     my $bban = shift.'00';
     my $rest = 0;
@@ -66,17 +82,20 @@ sub compute_key (_) {
     $key;
 }
 
-##
-# Computes and returns the Basic Bank Account Number (BBAN) corresponding to the
-# given bank identifier, bank location identifier and the account identifier.
-#
-# @param bank_id            The bank identifier.
-# @param bank_locaiton_id   The bank location identifier.
-# @param account_id         The account identifier.
-#
-# @return A string representation of the Basic Bank Account Number (BBAN) with
-# the key part. The returned BBAN can contains alpha and digit characters.
-##
+=head2 get_BBAN ( $bank_id, $bank_location_id, $account_id )
+
+Computes and returns the Basic Bank Account Number (BBAN) corresponding to the
+given bank identifier, bank location identifier and the account identifier. When
+
+    $bank_id            is the bank identifier.
+    $bank_locaiton_id   is the bank location identifier.
+    $account_id         is the account identifier.
+
+return the string representation of the Basic Bank Account Number (BBAN) with
+the key part. The returned BBAN can contains alpha and digit characters.
+
+=cut
+
 sub get_BBAN {
     my $bank_id = shift;
     my $bank_location_id = shift;
